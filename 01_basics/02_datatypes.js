a8b53eec4dd45a0ace6065076b940fa96d3fbac3
Kshama Tomer <114792238+Kshama1234@users.noreply.github.com>
2024-06-01 07:57:40 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js as new version
+
+//alert(4+3) //we are using nodejs, not browser
+
+
+let name = "Kshama"
+let age = 19
+let itLogged = false
+
+// number => 2 to the power 53
+//bigint
+
+//boolean = true or false
+//null = stand alone value (type = object)
+//undefined = kindda place holder for a value which is not defined yet (type = undefined)
+//symbol = uniquness
+
+//object
+console.log(typeof "Kshama")
+console.log(typeof age)
+console.log(typeof null)
